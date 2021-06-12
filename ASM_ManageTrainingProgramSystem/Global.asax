30582db56e6eb3d105a0f5009bc08a0f6702ea27
phuongtagcd19842 <66770565+phuongtagcd19842@users.noreply.github.com>
2021-06-12 23:45:33 +0700

    Add project files.

---
+++ ASM_ManageTrainingProgramSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASM_ManageTrainingProgramSystem.MvcApplication" Language="C#" %>
---
+++ ASM_ManageTrainingProgramSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASM_ManageTrainingProgramSystem.Startup))]
+namespace ASM_ManageTrainingProgramSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
