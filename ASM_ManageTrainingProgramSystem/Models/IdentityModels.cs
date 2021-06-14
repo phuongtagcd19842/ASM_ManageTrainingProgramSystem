1c484968d9df72b72f2503b409b222eccb771759
Nguyen Hoang <hoangngcd18786@fpt.edu.vn>
2021-06-15 04:25:08 +0700

    Add TrainerInfo Model and Enable migration

--- ASM_ManageTrainingProgramSystem/Models/IdentityModels.cs
+++ ASM_ManageTrainingProgramSystem/Models/IdentityModels.cs
@@ -25,6 +25,8 @@ namespace ASM_ManageTrainingProgramSystem.Models
         {
         }
 
+        public DbSet<TrainerInfo> TrainersInfo { get; set; }
+
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
---
+++ ASM_ManageTrainingProgramSystem/Models/TrainerInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ASM_ManageTrainingProgramSystem.Models
+{
+    public class TrainerInfo
+    {
+        [Key]
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+        public string TrainerName { get; set; }
+        public int ExternalOrInternal { get; set; }
+        public string WorkingPlace { get; set; }
+        public string Telephone { get; set; }
+        public string EmailAddress { get; set; }
+
+    }
+}
