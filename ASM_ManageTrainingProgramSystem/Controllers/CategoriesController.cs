b309ba3153ccdd4dccfa4085f1fb82b89ab9d1ba
phuongtagcd19842 <66770565+phuongtagcd19842@users.noreply.github.com>
2021-06-17 16:04:28 +0700

    Button Edit on Course Categories List page

--- ASM_ManageTrainingProgramSystem/Controllers/CategoriesController.cs
+++ ASM_ManageTrainingProgramSystem/Controllers/CategoriesController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -38,6 +39,33 @@ namespace ASM_ManageTrainingProgramSystem.Controllers
             };
 
             _context.Categories.Add(newCategory);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var categoryInDb = _context.Categories.SingleOrDefault(c => c.Id == id);
+            if (categoryInDb == null) return HttpNotFound();
+            Category category = categoryInDb;
+            return View(category);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Category category)
+        {
+            if (!ModelState.IsValid) return View(category);
+            
+            var categoryInDb = _context.Categories.SingleOrDefault(c => c.Id == category.Id);
+
+            if (categoryInDb == null) return HttpNotFound();
+
+            categoryInDb.CategoryName = category.CategoryName;
+            categoryInDb.Description = category.Description;
+
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
