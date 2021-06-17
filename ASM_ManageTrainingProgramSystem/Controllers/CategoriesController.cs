d917fd8a359580e147ada30d2626f142e23f587b
phuongtagcd19842 <66770565+phuongtagcd19842@users.noreply.github.com>
2021-06-17 10:36:21 +0700

    Add CategoriesController and Index view

---
+++ ASM_ManageTrainingProgramSystem/Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using ASM_ManageTrainingProgramSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASM_ManageTrainingProgramSystem.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private ApplicationDbContext _context;
+        public CategoriesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Categories
+        public ActionResult Index()
+        {
+            var categories = _context.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
