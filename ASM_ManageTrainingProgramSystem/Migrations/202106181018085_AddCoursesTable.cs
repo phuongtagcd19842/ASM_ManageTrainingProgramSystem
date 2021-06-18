49cbc0318d97536b76a736fdbc51974f32a5eb0e
phuongtagcd19842 <66770565+phuongtagcd19842@users.noreply.github.com>
2021-06-18 17:20:01 +0700

    Add Courses table and CourseCategoriesViewModel

---
+++ ASM_ManageTrainingProgramSystem/Migrations/202106181018085_AddCoursesTable.cs
@@ -0,0 +1,32 @@
+namespace ASM_ManageTrainingProgramSystem.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddCoursesTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Courses",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        CourseName = c.String(nullable: false, maxLength: 255),
+                        Description = c.String(nullable: false, maxLength: 255),
+                        CategoryId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Categories", t => t.CategoryId, cascadeDelete: true)
+                .Index(t => t.CategoryId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Courses", "CategoryId", "dbo.Categories");
+            DropIndex("dbo.Courses", new[] { "CategoryId" });
+            DropTable("dbo.Courses");
+        }
+    }
+}
---
+++ ASM_ManageTrainingProgramSystem/Models/Course.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using ASM_ManageTrainingProgramSystem.Models;
+using ASM_ManageTrainingProgramSystem.UniqueAttribute;
+
+namespace ASM_ManageTrainingProgramSystem.Models
+{
+    public class Course
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        [Unique(ErrorMessage = "Course already exists!")]
+        public string CourseName { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Description { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } //link object
+    }
+}
