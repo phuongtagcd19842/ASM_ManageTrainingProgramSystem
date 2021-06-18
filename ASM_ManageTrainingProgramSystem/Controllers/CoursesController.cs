4d643d45b0becef01fcf035925bc4e317c785162
phuongtagcd19842 <66770565+phuongtagcd19842@users.noreply.github.com>
2021-06-18 17:29:10 +0700

    Courses List page and button Create a new Course

---
+++ ASM_ManageTrainingProgramSystem/Controllers/CoursesController.cs
@@ -0,0 +1,64 @@
+﻿using ASM_ManageTrainingProgramSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using ASM_ManageTrainingProgramSystem.ViewModels;
+
+namespace ASM_ManageTrainingProgramSystem.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private ApplicationDbContext _context;
+        public CoursesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Courses
+        public ActionResult Index()
+        {
+            var courses = _context.Courses
+                .Include(c => c.Category)
+                .ToList();
+            return View(courses);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            var viewModel = new CourseCategoriesViewModel()
+            {
+                Categories = _context.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Course course)
+        {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CourseCategoriesViewModel()
+                {
+                    Course = course,
+                    Categories = _context.Categories.ToList()
+                };
+                return View(viewModel);
+            }
+
+            var newCourse = new Course()
+            {
+                CourseName = course.CourseName,
+                CategoryId = course.CategoryId,
+                Description = course.Description,
+            };
+
+            _context.Courses.Add(newCourse);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
