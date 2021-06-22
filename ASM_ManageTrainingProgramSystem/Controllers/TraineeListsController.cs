2c63628efe4f0ebd6f680069a82eebb832dff900
phuongtagcd19842 <66770565+phuongtagcd19842@users.noreply.github.com>
2021-06-22 23:47:55 +0700

    Button Details/Edit/Delete on Trainees List page

--- ASM_ManageTrainingProgramSystem/Controllers/TraineeListsController.cs
+++ ASM_ManageTrainingProgramSystem/Controllers/TraineeListsController.cs
@@ -35,5 +35,61 @@ namespace ASM_ManageTrainingProgramSystem.Controllers
             }    
             return View(Trainees);
         }
+
+        public ActionResult Details(string id)
+        {
+            if (id == null) return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+
+            var traineeInfo = _context.TraineesInfo.SingleOrDefault(t => t.UserId.Equals(id));
+            if (traineeInfo == null) return HttpNotFound();
+            return View(traineeInfo);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(string id)
+        {
+            if (id == null) return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+
+            var traineeInfo = _context.TraineesInfo.SingleOrDefault(t => t.UserId.Equals(id));
+            if (traineeInfo == null) return HttpNotFound();
+            return View(traineeInfo);
+        }
+        [HttpPost]
+        public ActionResult Edit(TraineeInfo traineeInfo)
+        {
+            if (traineeInfo == null) return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            var traineeInfoInDb = _context.TraineesInfo.SingleOrDefault(t => t.UserId.Equals(traineeInfo.UserId));
+
+            traineeInfoInDb.TraineeName = traineeInfo.TraineeName;
+            traineeInfoInDb.Age = traineeInfo.Age;
+            traineeInfoInDb.DateOfBirth = traineeInfo.DateOfBirth;
+            traineeInfoInDb.Education = traineeInfo.Education;
+            traineeInfoInDb.ProgrammingLanguage = traineeInfo.ProgrammingLanguage;
+            traineeInfoInDb.TOEICScore = traineeInfo.TOEICScore;
+            traineeInfoInDb.ExperienceDetail = traineeInfo.ExperienceDetail;
+            traineeInfoInDb.Department = traineeInfo.Department;
+            traineeInfoInDb.Location = traineeInfo.Location;
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(string id)
+        {
+            if (id == null) return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+
+            var user = _context.Users.SingleOrDefault(u => u.Id.Equals(id));
+            var traineeInfo = _context.TraineesInfo.SingleOrDefault(u => u.UserId.Equals(id));
+            
+            if (user == null) return HttpNotFound();
+  
+            _context.TraineesInfo.Remove(traineeInfo);
+            _context.Users.Remove(user);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
     }
 }
