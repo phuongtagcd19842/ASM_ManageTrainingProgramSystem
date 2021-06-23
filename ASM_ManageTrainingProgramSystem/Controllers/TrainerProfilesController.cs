ebffddbc328d3efc2b8869ca675c7484dedec788
A Pun <anhphuongtran01ql@gmail.com>
2021-06-23 12:59:14 +0700

    TrainerProfiles Controller and Index page

---
+++ ASM_ManageTrainingProgramSystem/Controllers/TrainerProfilesController.cs
@@ -0,0 +1,61 @@
+﻿using ASM_ManageTrainingProgramSystem.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASM_ManageTrainingProgramSystem.Controllers
+{
+    public class TrainerProfilesController : Controller
+    {
+        private ApplicationDbContext _context;
+        private UserManager<ApplicationUser> _userManager;
+        public TrainerProfilesController()
+        {
+            _context = new ApplicationDbContext();
+            _userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+        }
+
+        // GET: TrainerProfiles
+        public ActionResult Index()
+        {
+            var trainerInfo = _context.TrainersInfo.ToList();
+
+            if (trainerInfo == null) return HttpNotFound();
+
+            return View(trainerInfo);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(string id)
+        {
+            if (id == null) return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+
+            var trainerInfo = _context.TrainersInfo.SingleOrDefault(t => t.UserId.Equals(id));
+            if (trainerInfo == null) return HttpNotFound();
+
+            return View(trainerInfo);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(TrainerInfo trainerInfo)
+        {
+            var trainerInfoInDb = _context.TrainersInfo.SingleOrDefault(t => t.UserId.Equals(trainerInfo.UserId));
+            if (trainerInfo == null) return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+
+            trainerInfoInDb.TrainerName = trainerInfo.TrainerName;
+            trainerInfoInDb.ExternalOrInternal = trainerInfo.ExternalOrInternal;
+            trainerInfoInDb.WorkingPlace = trainerInfo.WorkingPlace;
+            trainerInfoInDb.Telephone = trainerInfo.Telephone;
+            trainerInfoInDb.EmailAddress = trainerInfo.EmailAddress;
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
